ff77e5bd853936bc376be7d5f5a7876c116f81b3
p0bs <robin_penfold@yahoo.com>
2021-01-03 15:39:15 +0000

    Adding UTF code for pound sign so as to avoid warning. Adding error checks

--- R/get_price_Zoopla.R
+++ R/get_price_Zoopla.R
@@ -7,21 +7,51 @@
 #' @examples
 #' \dontrun{
 #' get_price_Zoopla(link_house = 'https://www.zoopla.co.uk/property/1-oak-road/reigate/rh2-0bp/19534314')
+#' get_price_Zoopla(link_house = 'https://www.zoopla.co.uk/property/10-waverleigh-road/cranleigh/gu6-8bz/9852441')
 #' }
 
 get_price_Zoopla <- function(link_house){
   
-  # Add error checks
+  # Add error checks, assuming that there are no houses under GBP 1k
   
-  data <- xml2::read_html(link_house) %>% 
+  if (!is.character(link_house)){ 
+    rlang::abort("link_house must be a character input") 
+  } 
+  
+  if (length(link_house) != 1){ 
+    rlang::abort("You must enter one, and only one, entry for link_house") 
+  } 
+  
+  if (stringr::str_sub(link_house, start = 1L, end = 34L) != "https://www.zoopla.co.uk/property/"){
+    rlang::abort("link_house must begin with 'https://www.zoopla.co.uk/property/'")
+  }
+  
+  # Z <- tibble::tibble(links = c(link_house, link_house1)) %>% dplyr::rowwise() %>% dplyr::mutate(price = get_price_Zoopla(link_house = links))
+
+  detail <- xml2::read_html(link_house) %>% 
     rvest::html_nodes(".pdp-estimate__price") %>%
     rvest::html_text() %>% 
-    stringr::str_trim()
+    stringr::str_split(., "\u00A3") %>% 
+    .[[1]] %>% 
+    .[2]
   
-  value_house <-  stringr::word(data, 3)[1] %>% 
-    stringr::str_sub(2, -2) %>% 
+  detail_first <- stringr::str_sub(
+    string = detail, 
+    start = 1L,
+    end = -2L) %>% 
     as.numeric()
   
-  return(value_house)
+  detail_last <- stringr::str_sub(
+    string = detail, 
+    start = -1L,
+    end = -1L)
+  
+  value_out <- detail_first * dplyr::case_when(
+    detail_last == "k" ~ 1000,
+    detail_last == "m" ~ 1000000,
+    TRUE ~ NA_real_
+  )
+  
+  return(value_out)
   
 }
