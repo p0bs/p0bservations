977a0d722c0899e0c8964c8fbe7cd4ad14603952
p0bs <robin_penfold@yahoo.com>
2022-05-21 13:07:37 +0100

    Updating calc_income_net to include a year of analysis, using the mutliple years now in tax_parameters

--- R/calc_income_net.R
+++ R/calc_income_net.R
@@ -1,16 +1,20 @@
 #' @title Calculate income net of UK tax and National Insurance
 #'
-#' @description This function applies the prevailing tax bands and rates to give a simple calculation for income after UK tax and National Insurance (to the nearest couple of pounds) for FY20/21.
+#' @description This function applies the prevailing tax bands and rates to give a simple calculation for income after UK tax and National Insurance (to the nearest couple of pounds) for FY20/21. Please note that these are approximations, so do not rely on them for financial returns or planning.
 #' @param income_taxable The taxable income level (i.e. after deductions for things like pension contributions).
+#' @param tax_year_end The calendar year in which the tax year ends, as a YYYY integer. For example, tax year 2020/21 would be 2021L (which is the default value).
 #' @keywords Tax
 #' @export
 #' @examples
 #' \dontrun{
-#' calc_income_net(income_taxable = 38000)
+#' calc_income_net(income_taxable = 38000, tax_year_end = 2022L)
 #' }
+#' 
+#' @importFrom rlang .data
 
-calc_income_net <- function(income_taxable){
+calc_income_net <- function(income_taxable, tax_year_end = 2021L){
   
+  # Error checks ----
   if (!is.numeric(income_taxable)){ 
     rlang::abort("income_taxable must be a numeric input") 
   } 
@@ -23,89 +27,88 @@ calc_income_net <- function(income_taxable){
     rlang::abort("income_taxable must be positive")
   }
   
-  # Add tax limits, rates and breakpoints
-  level_ni_lower <- 9500
-  level_ni_upper <- 50000
-  level_allowance_lower <- 12500
-  level_allowance_upper <- 100000
-  level_tax_higher <- 50000
-  level_tax_upper <- 150000
-  rate_ni_lower <- 0.12
-  rate_ni_upper <- 0.02
-  rate_allowance_drop <- 0.5
-  rate_tax_basic <- 0.2
-  rate_tax_higher <- 0.4
-  rate_tax_upper <- 0.45
-  rate_tax_sales <- 0.2
+  if (!is.integer(tax_year_end)){ 
+    rlang::abort("tax_year_end must be an integer input") 
+  } 
   
-  # Tax calculations
-  income_allowance <- max(
-    0, 
-    level_allowance_lower - 0.5 * max(
-      income_taxable - level_allowance_upper, 
-      0
-      )
+  year_tax_end_options <- p0bservations::tax_parameters |> 
+    dplyr::distinct(.data$year_tax_end) |> 
+    dplyr::pull(.data$year_tax_end)
+  
+  if (!(tax_year_end %in% year_tax_end_options)){ 
+    rlang::abort("tax_year_end is not in our dataset. Please choose another entry.") 
+  } 
+  
+  # Add tax limits, rates and breakpoints ----
+  tax_FY <- p0bservations::tax_parameters |> 
+    dplyr::filter(.data$year_tax_end == tax_year_end)
+  
+
+  # Tax calculations ----
+  tax_calculations <- tax_FY |> 
+    dplyr::rowwise() |> 
+    dplyr::mutate(
+      income_allowance = max(
+        0, 
+        .data$level_allowance_lower - 0.5 * max(
+          income_taxable - .data$level_allowance_upper, 
+          0
+        )
+      ),
+      taxable_ni_lower = max(
+        0, 
+        (min(
+          income_taxable, 
+          .data$level_ni_upper
+        ) - .data$level_ni_lower)
+      ),
+      taxable_ni_higher = max(
+        0, 
+        (income_taxable - .data$level_ni_upper)
+      ),
+      allowance_drop = .data$rate_allowance_drop * max(
+        income_taxable - .data$level_allowance_upper, 
+        0
+      ),
+      taxable_zero = max(
+        0, 
+        .data$level_allowance_lower - max(
+          .data$allowance_drop, 
+          0
+        )
+      ),
+      excess_zero = max(
+        0, 
+        (income_taxable - .data$taxable_zero)
+      ),
+      taxable_basic = min(
+        .data$excess_zero, 
+        (.data$level_tax_higher - .data$level_allowance_lower)
+      ),
+      excess_basic = max(
+        0, 
+        (income_taxable - .data$taxable_zero - .data$taxable_basic)
+      ),
+      taxable_higher = min(
+        .data$excess_basic, 
+        (.data$level_tax_upper - .data$level_tax_higher + .data$level_allowance_lower)
+      ),
+      taxable_additional = max(
+        0, 
+        (income_taxable - .data$taxable_zero - .data$taxable_basic - .data$taxable_higher)
+      ),
+      tax = (.data$rate_tax_basic * .data$taxable_basic) + 
+        (.data$rate_tax_higher * .data$taxable_higher) + 
+        (.data$rate_tax_upper * .data$taxable_additional),
+      ni = (.data$rate_ni_lower * .data$taxable_ni_lower) + 
+        (.data$rate_ni_upper * .data$taxable_ni_higher),
+      income_net = income_taxable - .data$tax - .data$ni
     )
   
-  taxable_ni_lower <- max(
-    0, 
-    (min(
-      income_taxable, 
-      level_ni_upper
-      ) - level_ni_lower)
-    )
-  taxable_ni_higher <- max(
-    0, 
-    (income_taxable - level_ni_upper)
-    )
+  # Output ----
+  tax_output <- tax_calculations |> 
+    dplyr::pull(.data$income_net)
   
-  allowance_drop <- rate_allowance_drop * max(
-    income_taxable - level_allowance_upper, 
-    0
-    )
-  
-  taxable_zero <- max(
-    0, 
-    level_allowance_lower - max(
-      allowance_drop, 
-      0
-      )
-    )
-  
-  excess_zero <- max(
-    0, 
-    (income_taxable - taxable_zero)
-    )
-  
-  taxable_basic <- min(
-    excess_zero, 
-    (level_tax_higher - level_allowance_lower)
-    )
-  
-  excess_basic <- max(
-    0, 
-    (income_taxable - taxable_zero - taxable_basic)
-    )
-  
-  taxable_higher <- min(
-    excess_basic, 
-    (level_tax_upper - level_tax_higher + level_allowance_lower)
-    )
-  
-  taxable_additional <- max(
-    0, 
-    (income_taxable - taxable_zero - taxable_basic - taxable_higher)
-    )
-  
-  tax <- (rate_tax_basic * taxable_basic) + 
-    (rate_tax_higher * taxable_higher) + 
-    (rate_tax_upper * taxable_additional)
-  
-  ni <- (rate_ni_lower * taxable_ni_lower) + 
-    (rate_ni_upper * taxable_ni_higher)
-  
-  income_net <- income_taxable - tax - ni
-  
-  return(income_net)
+  return(tax_output)
   
 }
