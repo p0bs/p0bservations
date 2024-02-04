67eb39dff4ac230f964a72494f8036ef936e19ad
p0bs <robin_penfold@yahoo.com>
2024-02-04 13:49:11 +0000

    Renaming and refocusing the calc_income_net to consider only the tax liabilities, so that it can be more easily expanded

--- tests/testthat/test-liability_tax.R
+++ tests/testthat/test-liability_tax.R
@@ -1,21 +1,21 @@
 test_that("error checks work", {
   
   expect_error(
-    calc_income_net(
+    liability_tax(
       income_taxable = "15000",
       tax_year_end = 2024L
     )
   )
 
   expect_error(
-    calc_income_net(
+    liability_tax(
       income_taxable = c(9000, 29000),
       tax_year_end = 2024
     )
   )
   
   expect_error(
-    calc_income_net(
+    liability_tax(
       income_taxable = -20000,
       tax_year_end = 2024
     )
@@ -27,7 +27,7 @@ test_that("calculations work", {
   
   expect_true(
     is.numeric(
-      calc_income_net(
+      liability_tax(
         income_taxable = 20000,
         tax_year_end = 2024
       )
@@ -35,59 +35,59 @@ test_that("calculations work", {
   )
   
   expect_equal(
-    object = calc_income_net(
+    object = liability_tax(
       income_taxable = 8000,
       tax_year_end = 2024
       ), 
-    expected = 8000, 
-    tolerance = 0.0001
+    expected = 8000 - 8000, 
+    tolerance = 0.001
   )
   
   # Calculations taken from a third-party app
   
   expect_equal(
-    object = calc_income_net(
+    object = liability_tax(
       income_taxable = 22000, 
       tax_year_end = 2024
     ),
-    expected = 18984.08, 
-    tolerance = 0.0001
+    expected = 22000 - 18984.08, 
+    tolerance = 0.001
   )
   
   expect_equal(
-    object = calc_income_net(
+    object = liability_tax(
       income_taxable = 40000, 
       tax_year_end = 2024
       ),
-    expected = 31224.08, 
-    tolerance = 0.0001
+    expected = 40000 - 31224.08, 
+    tolerance = 0.001
   )
   
   expect_equal(
-    object = calc_income_net(
+    object = liability_tax(
       income_taxable = 65000, 
       tax_year_end = 2024
     ),
-    expected = 46749.68, 
-    tolerance = 0.0001
+    expected = 65000 - 46749.68, 
+    tolerance = 0.001
   )
   
   expect_equal(
-    object = calc_income_net(
+    object = liability_tax(
       income_taxable = 105000, 
       tax_year_end = 2024
     ),
-    expected = 68949.68, 
-    tolerance = 0.0001
+    expected = 105000 - 68949.68, 
+    tolerance = 0.001
   )
   
   expect_equal(
-    object = calc_income_net(
+    object = liability_tax(
       income_taxable = 160000, 
       tax_year_end = 2024
     ),
-    expected = 95078, 
-    tolerance = 0.0001
+    expected = 160000 - 95078, 
+    tolerance = 0.001
   )
   
 })
